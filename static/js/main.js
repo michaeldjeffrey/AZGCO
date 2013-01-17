8ee653f1b2c74e2452f7877d57e08461bc8b2a1e
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-01-17 15:43:43 -0700

    Successfully pass make and model to next page
    was passing just the first part of the string of the model

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 	$.getJSON('/static/json/cars.json', function(data){
 		for(var i = 0; i < data.length; i++){
-			$("#make").append('<option value='+i+'>'+data[i].title+'</option>');
+			$("#make").append('<option data-number='+i+' value='+data[i].title+'>'+data[i].title+'</option>');
 		}
 	});
 	$("#make").live("change", function(){
 		self = this;
+		var selected = $(self).find('option:selected');
+		var make = selected.data('number');
 		
 		$.getJSON('/static/json/cars.json', function(data){
 			$("#model").empty().append('<option>select model</option>');
-			for(var k = 0; k < data[self.value].models.length; k++){
-				$("#model").append('<option value='+data[self.value].models[k].title+'>'+data[self.value].models[k].title+'</option>');
+			for(var k = 0; k < data[make].models.length; k++){
+				$("#model").append('<option value="'+data[make].models[k].title+'">'+data[make].models[k].title+'</option>');
 			}
 		});
 	});
