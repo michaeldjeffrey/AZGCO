3e452d61feaf572e88c3260f679dce44b9bcf575
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-03-08 18:35:35 -0700

    new initial commit, haven't committed in a while
    lots of functionality added

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,73 @@
 $(document).ready(function(){
+		$('#make').chosen();
+		$('#model').chosen();
+
+	var scheduleAppointmentAvailable = false;
+
 	$.getJSON('/static/json/cars.json', function(data){
 		for(var i = 0; i < data.length; i++){
-			$("#make").append('<option data-number='+i+' value='+data[i].title+'>'+data[i].title+'</option>');
+			$('#make').append('<option data-number='+i+
+				' value='+data[i].title+
+				'>'+data[i].title+'</option>').trigger('liszt:updated');
 		}
 	});
-	$("#make").live("change", function(){
+//==============================================================
+	$('#make').on('change', function(){
 		self = this;
 		var selected = $(self).find('option:selected');
 		var make = selected.data('number');
-		
+		dbMake = selected.val();
+
 		$.getJSON('/static/json/cars.json', function(data){
-			$("#model").empty().append('<option>select model</option>');
+			$('#model').empty();
 			for(var k = 0; k < data[make].models.length; k++){
-				$("#model").append('<option value="'+data[make].models[k].title+'">'+data[make].models[k].title+'</option>');
+				$('#model')
+					.append('<option value="'+data[make].models[k].title+'">'+data[make].models[k].title+'</option>')
+					.val('')
+					.trigger('liszt:updated')
 			}
 		});
 	});
+//==============================================================
+	$('#btnQuote').on('click', function(){
+
+		if($('#year').val() != ''){
+			if(scheduleAppointmentAvailable == false){
+				var randQuote = Math.floor(Math.random() * (800 - 200 + 1)) + 200;
+				$('#quoteDisplay').html(randQuote+'.00');
+				$('#quoteHidden').val(randQuote+'.00');
+				scheduleAppointmentAvailable = true;
+			}
+		}
+	});
+//==============================================================
+	$('#scheduleAppointment').on('click',function(e){
+		if(scheduleAppointmentAvailable == false){
+			e.stopPropagation();
+			e.preventDefault();
+		}
+	});
+
+	$('#finalCheck').on('click', function(){
+		$('#dbCheckMake').html($('[name=make]').val());
+		$('#dbCheckModel').html($('[name=model]').val());
+		$('#dbCheckYear').html($('[name=year]').val());
+		$('#dbCheckServiceType').html($('[name=serviceType]').val());
+		$('#dbCheckLocation').html($('[name=location]').val());
+		$('#dbCheckName').html($('[name=name]').val());
+		$('#dbCheckEmail').html($('[name=email]').val());
+		$('#dbCheckPhone').html($('[name=phone]').val());
+		$('#dbCheckContactMethod').html($('[name=contactMethod]').val());
+		$('#dbCheckAppointmentTime').html($('[name=appointmentTime]').val());
+	});
+//==============================================================
+	var currentOpenField;
+		$('.buttonHiddenInfo').on('click', function(){
+			if(currentOpenField != $(this).attr('id')){
+				$(".hiddenInfo").slideUp();
+			}
+			currentOpenField = $(this).attr('id');
+			$("#"+currentOpenField+"info").slideToggle();
+		});
+	
 });
