5d42c0a289b4b85ffc31447b8d1ee278d6bd5168
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-04-09 20:04:34 -0700

    changed default status back to pending

--- hello.py
+++ hello.py
@@ -14,30 +14,9 @@ db = Connection().test
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
-carsAmount = db.cars.count()
-
-@app.route("/test")
-def test():
-	post = {
-		'name': '',
-		'town': '',
-		'status': '',
-		'make': '',
-		'model': '',
-		'year': '',
-		'serviceType': '',
-		'location': '',
-		'': '',
-		'': '',
-		'': '',
-		'': '',
-	}
-
-	return render_template('jobs.html', posts = post)
-
 @app.route("/")
 def index():
-	return render_template('index.html', carsAmount=carsAmount)
+	return render_template('index.html')
 
 @app.route('/scheduleAppointment/', methods=['POST', 'GET'])
 @app.route('/scheduleAppointment', methods=['POST', 'GET'])
@@ -55,6 +34,7 @@ def makeNewAppointment():
 			'location'        : request.form['location'],
 			'contactMethod'   : request.form['contactMethod'],
 			'appointmentTime' : request.form['appointmentTime'],
+			'status' 					: 'Pending',
 			'timeSubmitted'   : {
 				'date': datetime.now().strftime('%Y-%m-%d'),
 				'time': datetime.now().strftime("%H:%M:%S")
@@ -62,51 +42,19 @@ def makeNewAppointment():
 			}
 		db.cars.insert(post)
 		return render_template('thankYou.html')
+	
 
-@app.route("/result/", methods=['POST', 'GET'])
-@app.route("/result", methods=['POST', 'GET'])
-def result():
-	if request.method == 'POST':
-		carsAmount += 1
-		make = request.form['make']
-		model = request.form['model']
-		resp = make_response(render_template('showCars.html', carsAmount=carsAmount, make=make, model=model, cars=db.cars.find()))
-		resp.set_cookie('make', make)
-		resp.set_cookie('model', model)
-		post = {
-			'make': make,
-			'model': model,
-			'datetime': {
-				'date': datetime.now().strftime('%Y-%m-%d'),
-				'time': datetime.now().strftime("%H:%M:%S")
-				}
-		}
-		db.cars.insert(post)
-		return resp
-	else:
-		return showdb()
-
-def showdb():
-	make = request.cookies.get('make')
-	model = request.cookies.get('model')
-	return render_template('showCars.html',carsAmount=carsAmount, make=make, model=model, cars = db.cars.find())
-
-@app.route('/users')
-def name():
-	return render_template('users.html', carsAmount=carsAmount, users=db.people.find())		
-
-@app.route('/admin/')	
-@app.route('/admin')
-def admin():
-	return render_template('admin.html')	
-
+@app.route("/admin")
 @app.route("/jobs")
 def jobs():
-	return render_template('jobs.html', posts = db.cars.find())
+	return render_template('jobs.html', posts = db.cars.find().sort([('status', -1),("$natural", -1)]))
 
 @app.route("/jobs/<string:state>/<string:objid>")
 def job_action(state, objid):
-	db.cars.update({'_id': ObjectId(objid)}, {"$set": {'status':state}})
+	if state == 'delete':
+		db.cars.remove({'_id': ObjectId(objid)})
+	else:
+		db.cars.update({'_id': ObjectId(objid)}, {"$set": {'status':state}})
 	return redirect(url_for('jobs'))
 	# return "%s, %s" % (objid, state)
 
